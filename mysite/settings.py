d07121c575b7e95591172c3f11943b5b9c8ce8be
hamza shafique <hamza.shafique1@arbisoft.com>
2021-06-22 13:45:18 +0500

    hide secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^94a5hq37!o(b2slb&pgrr=u+_p930064-%cn+f_6^m%ov8g3='
+SECRET_KEY = 'CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead).'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
