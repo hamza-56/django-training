0ab8faf5db407a6fe259b1aa4642cc0237975af9
hamza shafique <hamza.shafique1@arbisoft.com>
2021-06-23 15:41:46 +0500

    Remove unused import

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 
