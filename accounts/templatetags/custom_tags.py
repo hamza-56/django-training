e944541acf340fdc8431d203bcd591552d917bbb
hamza shafique <hamza.shafique1@arbisoft.com>
2021-06-25 15:48:28 +0500

    Add logs info on homepage

--- accounts/templatetags/custom_tags.py
+++ accounts/templatetags/custom_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from accounts.models import User
+from accounts.models import Log, User
 
 register = template.Library()
 
@@ -8,3 +8,13 @@ register = template.Library()
 @register.simple_tag
 def users_count():
     return User.objects.count()
+
+
+@register.simple_tag
+def pst_logs_count():
+    return Log.objects.filter(timezone='Asia/Karachi').count()
+
+
+@register.simple_tag
+def utc_logs_count():
+    return Log.objects.filter(timezone='UTC').count()
