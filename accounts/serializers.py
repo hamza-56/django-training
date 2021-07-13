01830f8d5df37292af88762e91dc404fbef22767
hamza shafique <hamza.shafique1@arbisoft.com>
2021-07-13 12:00:12 +0500

    training task # 5

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,7 @@
 import pytz
 from rest_framework import serializers
 
-from .models import Log
+from .models import Log, User
 
 
 class LogSerializer(serializers.HyperlinkedModelSerializer):
@@ -14,3 +14,30 @@ class LogSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Log
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'password',
+        )
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'birth_date',
+            'bio',
+        )
--- api/views.py
+++ api/views.py
@@ -1,35 +1,37 @@
+from django.db.models.query import QuerySet
 from rest_framework import authentication, mixins, permissions, viewsets
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from accounts.models import Log, User
-from accounts.serializers import LogSerializer
-from todolist import serialzers
+from accounts.serializers import (LogSerializer, UserProfileSerializer,
+                                  UserSerializer)
 from todolist.models import TodoListItem
 from todolist.serialzers import TodoListItemSerializer
 
 
 class LogViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = (permissions.IsAuthenticated, )
+    authentication_classes = (authentication.TokenAuthentication, )
     queryset = Log.objects.all()
     serializer_class = LogSerializer
 
 
 class ListUsers(APIView):
-    """
+    '''
     View to list all users in the system.
 
     * Requires token authentication.
     * Only admin users are able to access this view.
-    """
-    permission_classes = [permissions.IsAdminUser]
-    authentication_classes = [authentication.TokenAuthentication]
+    '''
+    permission_classes = (permissions.IsAdminUser, )
+    authentication_classes = (authentication.TokenAuthentication, )
 
     def get(self, request, format=None):
-        """
+        '''
         Return a list of all users.
-        """
+        '''
         usernames = [user.username for user in User.objects.all()]
         return Response(usernames)
 
@@ -38,8 +40,8 @@ class TodoListViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
                       mixins.UpdateModelMixin, mixins.DestroyModelMixin,
                       mixins.ListModelMixin, viewsets.GenericViewSet):
 
-    permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = (permissions.IsAuthenticated, )
+    authentication_classes = (authentication.TokenAuthentication, )
     serializer_class = TodoListItemSerializer
 
     def get_queryset(self):
@@ -49,3 +51,25 @@ class TodoListViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
     def perform_create(self, serializer):
         user = self.request.user
         serializer.save(owner=user)
+
+
+class RegisterView(CreateAPIView):
+    model = User
+    serializer_class = UserSerializer
+
+
+class UserIsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
+
+
+class UserProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = (
+        permissions.IsAuthenticated,
+        UserIsOwnerOrReadOnly,
+    )
+    authentication_classes = [authentication.TokenAuthentication]
+    serializer_class = UserProfileSerializer
+    queryset = User.objects.all()
