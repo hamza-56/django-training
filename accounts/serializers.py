cafd8b66b3e1efbbdf8ccfb058a6728d96794c32
hamza shafique <hamza.shafique1@arbisoft.com>
2021-07-15 18:18:08 +0500

    Set hashed password explicitly in create

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,10 +14,10 @@ class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        '''
-        override create method to save hashed password
-        '''
-        return User.objects.create_user(**validated_data)
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
     class Meta:
         model = User
